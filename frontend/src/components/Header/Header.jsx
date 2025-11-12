3febfc8b1f3ed5883f4dfb1bf658d20dce326ffd
pankaj doley <151039433+doleypankaj@users.noreply.github.com>
2025-11-12 12:43:03 +0530

    added frontend files

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        
+        <div className="header">
+            <div className="header-contents">
+                <h2>Satisfy Your Cravings Anytime</h2>
+                <p>Explore a variety of mouth-watering dishes prepared with love and the freshest ingredients. Indulge in your favorites, delivered hot and fast.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
